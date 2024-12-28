1d15b82a6bd677519bf5513e9966d13d8af4be31
ZhaksybaiKazybek <zhaksybaikk@gmail.com>
2024-12-28 11:37:25 +0500

    Delete module3 directory

--- module3/js/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth Scroll for "Back to Top" Links
-document.querySelectorAll(".back-to-top").forEach(link => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      document.querySelector("#top").scrollIntoView({ behavior: "smooth" });
-    });
-  });
-  
